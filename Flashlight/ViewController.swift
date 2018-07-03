50e419500e0266622d599fe9175669c979e76bb0
zfrew <zmfrew@gmail.com>
2018-07-02 18:03:31 -0600

    add code to repo

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isOn = false
+    
+    @IBOutlet weak var buttonText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var rightGesture = UISwipeGestureRecognizer(target: self, action:#selector(self.respondToSwipeGesture))
+        rightGesture.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(rightGesture)
+        
+        let leftGesture = UISwipeGestureRecognizer(target: self, action:#selector(self.respondToSwipeGesture))
+        leftGesture.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(leftGesture)
+    }
+    
+    @objc func respondToSwipeGesture(gesture: UISwipeGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch (swipeGesture.direction) {
+            case .right:
+                view.backgroundColor = UIColor.white
+                buttonText.setTitle("Off", for: .normal)
+                buttonText.setTitleColor(UIColor.black, for: .normal)
+                isOn = true
+                
+            case .left:
+                view.backgroundColor = UIColor.black
+                buttonText.setTitle("On", for: .normal)
+                buttonText.setTitleColor(UIColor.white, for: .normal)
+                isOn = false
+                
+            default:
+                break
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func buttonTapped(_ sender: Any) {
 
+        if isOn == false {
+            view.backgroundColor = UIColor.white
+            buttonText.setTitle("Off", for: .normal)
+            buttonText.setTitleColor(UIColor.black, for: .normal)
+            isOn = true
+        } else if isOn == true {
+            view.backgroundColor = UIColor.black
+            buttonText.setTitle("On", for: .normal)
+            buttonText.setTitleColor(UIColor.white, for: .normal)
+            isOn = false
+        }
+    }
 
 }
-
